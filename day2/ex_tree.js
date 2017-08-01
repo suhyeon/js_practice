3288c19c3f0aa36a493dcc4c3e1f67acf90b712d
SuhyeonJo <flora12375@gmail.com>
2017-08-01 11:41:29 +0900

    javascript practice day2

---
+++ day2/ex_tree.js
@@ -0,0 +1,16 @@
+for (var i = 0; i < 5; i++){
+  console.log('*');
+  console.log('\n');
+}
+var str ='';
+for(var i = 0; i < 10 ; i++){
+  if(i%2==0){
+    str += i;
+  }
+}
+console.log(str);
+var result = '';
+for (var i = 0; i < 5; i++){
+ result += '*'; 
+  console.log(result);
+}
