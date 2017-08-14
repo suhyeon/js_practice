57ffb2e3123c0863cf0e3ba19120af8264cb371f
SuhyeonJo <flora12375@gmail.com>
2017-08-14 15:34:51 +0900

    javascript practice day7

--- day7/morning.js
+++ day7/morning.js
@@ -1,15 +1,24 @@
 //palindrome(팰린드롬/회문)은 왼쪽에서 오른쪽으로 읽은 다음, 오른쪽부터 왼쪽으로 다시 읽어도 똑같은 형태와 의미를 유지하는 문장이나 단어를 지칭한다. 인자로 전달한 문자열이 palindrome인지 검사하여 Boolean값을 반환하는 함수를 완성하라. 단, 반드시 1자 이상의 문자열을 인자로 전달한다.
-var str1 = '';
-var str2 = '';
+
 function checkPalindrom(str) {
+  /* //for문 이용
+  var str1 = '';
+  var str2 = '';
   for(var i = 0; i < str.length; i++){
     str1 += str.charAt(i);
   }
   for(var i = str.length; i >= 0; i--){
     str2 += str.charAt(i);
   }
-  return str1 == str2;
-
+  return str1 == str2;*/
+  if(str.length < 2){
+    return false; // 2단어이상이 되어야 의미가 있을 수 있다.
+  }else{
+    var split1 = str.split(''); //스트링을 배열화
+    var split2 = split1.reverse(); //배열을 역순정렬
+    var split3 = split2.join(''); //역순정렬한배열을 다시 스트링화
+    return str === split3; // 역순과 파라미터 비교
+  }
 }
 
 console.log(checkPalindrom('dad')); // true
--- day7/morning1.js
+++ day7/morning1.js
@@ -1,21 +1,25 @@
 //1차원의 점들이 주어졌을 때, 그 중 가장 거리가 짧은 것(들)의 쌍을 배열로 반환하는 함수를 작성하라. (단 점들의 배열은 모두 정렬되어있다고 가정한다.) 예를들어 [1, 3, 4, 8, 13, 17, 20, 23, 24]이 주어졌다면, 결과값은 [[3, 4], [23, 24]]가 될 것이다.
 
 function findMinDistance(array){
-  var arr = [];
-  /*for(var i = 0; i <= array.length ; i++){
-    for(var j = 0; j <= i; j++){
-      var result = array[i+1] - array[i];
-      arr.push(result);
-    }
-    arr.sort(function(a,b){return a-b;});
-  }*/
-  var result = array.reduce(function (previousValue, currentValue){
-    var string = currentValue - previousValue;
-    arr.push(string);
-    return currentValue;
+  var arr = []; //배열요소의 앞뒤 차이를 저장할 배열 초기화 
+  //정렬되어있다고 가정했기 때문에 이 방법 채택
+  var arr2 = []; //결과값이 될 배열 초기화
+
+  array.reduce(function (previousValue, currentValue){
+    var string = currentValue - previousValue; //배열 요소의 앞뒤차이를 string에 저장
+    arr.push(string); //string값을 배열에 push
+    return currentValue; //reduce 인자에 currentValue 리턴
   });
-  var some = array.filter(arr.Math.min(0));
-  
+
+  var line = Math.min.apply(null,arr); //arr의 요소값중 최소값을 line에 할당
+
+  for(var i = 0 ; i <= arr.length; i++){
+    if(arr[i]===line){ //최소값과 같은 값의 요소라면
+      var newarr = new Array(array[i],array[i+1]); //같은 인덱스의 파라미터배열의 앞뒤를 새로운 배열로 선언
+      arr2.push(newarr); //선언된 배열을 arr2에 삽입
+    }
+  }
+  return arr2; //최종값을 반환
 }
 
 // 1차원 점의 배열
